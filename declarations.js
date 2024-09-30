86eb249ed199f468d389d5699a7c26c9119242f8
yrahhaou <yassinerahhaoui12@gmail.com>
2024-09-30 16:19:26 +0100

    declarations task

---
+++ declarations.js
@@ -0,0 +1,18 @@
+const escapeStr = "`\\/\"'"
+const arr = [4, '2']
+const obj = {
+	str: "",
+	num: 10,
+	bool: boolean,
+	undef: undefined
+}
+const arrVal = [4, undefined, '2']
+const objVal = {
+	str: "",
+	num: 10,
+	bool: true
+}
+const nested = {
+	arr: arrVal,
+	obj: objVal
+}
