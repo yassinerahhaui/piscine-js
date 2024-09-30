c7bf205e20ae0f85f94df7f780099a8a167c5b3c
yrahhaou <yassinerahhaoui12@gmail.com>
2024-09-30 20:46:33 +0100

    more-or-less task

--- more-or-less.js
+++ more-or-less.js
@@ -1,4 +1,4 @@
-const more = (num) => num++;
-const less = (num) => num--;
+const more = (num) => num+=1;
+const less = (num) => num-=1;
 const add = (arg1, arg2) => arg1 + arg2;
 const sub = (arg1, arg2) => arg1 - arg2;
