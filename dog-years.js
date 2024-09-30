552055dcc906c092f7efc3dad290dcc4c3d8f01f
yrahhaou <yassinerahhaoui12@gmail.com>
2024-09-30 23:24:30 +0100

    dog-years task

--- dog-years.js
+++ dog-years.js
@@ -1,5 +1,5 @@
 const planets = {
-    earth: (1*60*60*24*365.25),
+    earth: (1 * 60 * 60 * 24 * 365.25),
     mercury: 0.2408467,
     venus: 0.61519726,
     mars: 1.8808158,
@@ -7,6 +7,6 @@ const planets = {
     saturn: 29.447498,
     uranus: 84.016846,
     neptune: 164.79132
-  };
+};
 
-const dogYears = (planet, seconds) => ((seconds / planets[planet]) * 7).toFixed(2);
+const dogYears = (planet, seconds) => parseFloat(((seconds / planets[planet]) * 7).toFixed(2));
