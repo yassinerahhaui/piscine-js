f226b63dbaf80bddbae8a6dbdbff0dbf0696c7be
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-01 03:00:41 +0100

    is task

---
+++ is.js
@@ -0,0 +1,11 @@
+is.num = (arg) => typeof arg === "number",
+is.nan = (arg) => typeof arg === "NaN",
+is.str = (arg) => typeof arg === "string",
+is.bool = (arg) => typeof arg === "boolean",
+is.undef = (arg) => typeof arg === "undefined",
+is.def = (arg) => typeof arg === "defined",
+is.arr = (arg) => Array.isArray(arg),
+is.obj = (arg) => !Array.isArray(arg) && typeof arg === "object",
+is.fun = (arg) => typeof arg === "function",
+is.truthy = (arg) => Boolean(arg),
+is.falsy = (arg) => !Boolean(arg)
