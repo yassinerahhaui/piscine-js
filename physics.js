739303baee69be2e6d1bad2be0327d73b3c8e447
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-01 05:19:47 +0100

    phisic task

--- physics.js
+++ physics.js
@@ -1,9 +1,10 @@
 const getAcceleration = (obj) => {
-    if ((obj.f / obj.m) === (obj.Δv / obj.Δt) && (obj.f / obj.m) === (obj.d *2 / obj.t * obj.t)) {
+    if (obj.f / obj.m) {
         return obj.f / obj.m
+    } else if (obj.Δv / obj.Δt) {
+        return obj.Δv / obj.Δt
+    } else if ((obj.d *2) / (obj.t * obj.t)) {
+        return (obj.d *2) / (obj.t * obj.t)
     }
     return "impossible"
 }
-// f / m = a
-// a = Δv / Δt
-// a = 
