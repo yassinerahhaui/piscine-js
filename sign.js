431abcf5efd44e60c81026267cfeb79215957054
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-01 02:09:08 +0100

    sameSign func

--- sign.js
+++ sign.js
@@ -1,2 +1,2 @@
 const sign = (num) => num === 0? 0 : num > 0 ? 1 : -1;
-const sameSign = (num1, num2) => num1 === num2 ? true : false;
+const sameSign = (num1, num2) => sign(num1) === sign(num2) ? true : false;
