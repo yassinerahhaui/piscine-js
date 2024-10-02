f739c7a8ad0874b8e920e31dc11d78b39b83dd9f
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-02 19:01:18 +0100

    elementary.js

--- elementary.js
+++ elementary.js
@@ -38,7 +38,7 @@ const divide = (a, b) => {
 }
 const modulo = (a, b) => {
     let negative = false
-    if (b >= 0 && a < 0) {
+    if (a < 0) {
         negative = true
     }
     a = abs(a); b = abs(b);
@@ -57,3 +57,4 @@ const modulo = (a, b) => {
 // console.log(divide(123, -22));
 console.log(modulo(-123, 22));
 console.log(modulo(123, -22));
+console.log(modulo(-123, -22));
