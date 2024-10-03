cd5fd9a81aa0dacdf9db75d4422e4a567c8bf5ea
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-03 06:08:05 +0100

    unbreakable.js

--- unbreakable.js
+++ unbreakable.js
@@ -23,19 +23,15 @@ const split = (str, sep) => {
     let result = []
     let start = 0
     let ln = sep.length
-    for (let i = 0; i + ln <= str.length; i++) {
+    for (let i = 0; i <= str.length; i++) {
         let item = slice(str, i, i + ln)
-        if (item === sep) {
+        if (item === sep || i === str.length) {
             let res = slice(str, start, i)
-            if (start !== i || i === 0) {
-                result = [...result, res]
-            }
+            result = [...result, res]
             start = i + ln
-            item = ""
+            i = start -1
         }
     }
-    let res = slice(str, start)
-    result = [...result, res]
     return result
 }
 const join = (arr, sep) => {
@@ -47,9 +43,9 @@ const join = (arr, sep) => {
     }
     return result
 }
-// console.log(split('rrirr', 'rr'));
-// console.log(split('ggg - ddd - b', ' - '));
-// console.log(split("a,q,s,x",","));
-// console.log(split('a b c', ' '));
-// console.log(split('rrrr', 'rr'));
-// console.log(split('ee,ff,g,', ','));
+console.log(split('rrirr', 'rr'));
+console.log(split('ggg - ddd - b', ' - '));
+console.log(split("a,q,s,x",","));
+console.log(split('a b c', ' '));
+console.log(split('rrrr', 'rr'));
+console.log(split('ee,ff,g,', ','));
