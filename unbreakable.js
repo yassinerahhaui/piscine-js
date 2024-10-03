ec53abcb75671db69562fc27039b006ed64dcfcc
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-03 04:38:35 +0100

    unbreakable.js

---
+++ unbreakable.js
@@ -0,0 +1,24 @@
+const split = (str, sep) => {
+    let result = []
+    let item = ""
+    for (let char of str) {
+        if (char === sep) {
+            result = [...result,item]
+            item = ""
+        } 
+        item += char
+    }
+    item !== "" ? result = [...result,item] : ""
+    return result
+}
+const join = (arr, sep) => {
+    let result = ""
+    let index = 1
+    for (let item of arr) {
+        index < arr.length ? result += item + sep : result += item
+        index++
+    }
+    return result
+}
+// console.log(join(["1","1","1","1",],","));
+// console.log(split("a,q,s,x",","));
