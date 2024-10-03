9c0488a243bdae92842d83693db379aa06282416
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-03 01:06:35 +0100

    index-of.js

--- slicer.js
+++ slicer.js
@@ -1,4 +1,4 @@
-const slice = (item, start, end = 0) => {
+const slice = (item, start, end = item.length) => {
     if (start < 0) {
         start = -start
         start = item.length - start
@@ -7,14 +7,15 @@ const slice = (item, start, end = 0) => {
         end = -end
         end = item.length - end
     }
-    if (end === 0) end = item.length - 1
+    // if (end === 0) end = item.length
     let arr = []
     for (let i = 0; i < item.length; i++) {
-        i >= start && i <= end ? arr.push(item[i]) : ''
+        i >= start && i < end ? arr.push(item[i]) : ''
     }
     if (typeof item === "string") {
         return arr.join("")
     }
     return arr
 }
-// console.log(slice('abcdef', -2));
+console.log(slice('abcdef', -2));
+console.log(slice('abcdef', 0, 2));
