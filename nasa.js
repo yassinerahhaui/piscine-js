e3fff0777062a93388d407a0016ca5936d8e96d7
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-03 06:36:55 +0100

    nasa.js

---
+++ nasa.js
@@ -0,0 +1,16 @@
+const nasa = (num) => {
+    let result = "" 
+    for (let i=1; i<=num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result += "NASA"
+        } else if (i % 3 === 0) {
+            result += "NA"
+        } else if (i % 5 === 0) {
+            result += "SA"
+        } else {
+            result += String(i)
+        }
+    }
+    return result
+}
+// console.log(nasa(20));
