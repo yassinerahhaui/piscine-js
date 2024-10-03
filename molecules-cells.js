ea756f2b80d3be2279f0f402a8d9aeadac7e3726
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-03 20:50:34 +0100

    molecules-cells.js

---
+++ molecules-cells.js
@@ -0,0 +1,17 @@
+const RNA = (str) => {
+    const rnaToDna = { C: 'G', G: 'C', A: 'T', U: 'A' }
+    let result = ""
+    for (let c of str) {
+        result += rnaToDna[c]
+    }
+    return result
+}
+
+const DNA = (str) => {
+    const dnaToRna = { G: 'C', C: 'G', T: 'A', A: 'U' }
+    let result = ""
+    for (let c of str) {
+        result += dnaToRna[c]
+    }
+    return result
+}
