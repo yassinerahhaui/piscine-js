1425dc222af90dacf3c0d879b6a8c6b2945d7e97
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-03 04:50:16 +0100

    unbreakable.js

--- unbreakable.js
+++ unbreakable.js
@@ -1,14 +1,34 @@
+const slice = (item, start, end = item.length) => {
+    if (start < 0) {
+        start = -start
+        start = item.length - start
+    }
+    if (end < 0) {
+        end = -end
+        end = item.length - end
+    }
+    // if (end === 0) end = item.length
+    let arr = []
+    for (let i = 0; i < item.length; i++) {
+        i >= start && i < end ? arr.push(item[i]) : ''
+    }
+    if (typeof item === "string") {
+        return arr.join("")
+    }
+    return arr
+}
 const split = (str, sep) => {
     let result = []
     let item = ""
-    for (let char of str) {
-        item += char
-        if (char === sep) {
-            result = [...result,item-sep]
+    for (let i = 0; i < str.length - sep.length; i++) {
+        if (slice(str, i, i + sep.length) === sep) {
+            result = [...result, item]
             item = ""
+        } else {
+            item += char
         }
     }
-    item !== "" ? result = [...result,item] : ""
+    item !== "" ? result = [...result, item] : ""
     return result
 }
 const join = (arr, sep) => {
