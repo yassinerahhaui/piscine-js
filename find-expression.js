ab940edea1fcaf445b0a69a48060cb87d8ce0873
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-05 04:29:46 +0100

    find-expression.js

--- find-expression.js
+++ find-expression.js
@@ -1,18 +1,11 @@
-const findExpression = (num, res= "1", sum = 1) => {
-    if (num%2 === 1) return "undefined";
+const findExpression = (num, res = "1", sum = 1) => {
+    if (num % 2 === 1) return "undefined";
     if (sum > num) {
         return "undefined"
     }
     if (sum === num) return res;
-    if ((sum * 2) < num) {
-        sum *= 2
-        res += ` ${mul2}`
-        return findExpression(num, res, sum)
-    } else if ((sum + 4) <= num) {
-        sum += 4
-        res += ` ${add4}`
-        return findExpression(num,res, sum);
-    }
+    return findExpression(num, res + ` ${add4}`, sum + 4) ||
+        findExpression(num, res + ` ${mul2}`, sum * 2)
 }
 // console.log(findExpression(8));
 // console.log(findExpression(14));
