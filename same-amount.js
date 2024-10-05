8de7eaf9b38596283eef2af4ff6c0d59d8469d59
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-06 00:29:15 +0100

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,7 @@
 const sameAmount = (str, reg1, reg2) => {
-    if (str.match(reg1) && str.match(reg2)) return true ;
+    let rg1 = str.match(reg1)
+    let rg2 = str.match(reg2)
+    if (rg1.length === rg2.length ) return true ;
     return false
 }
 // console.log(sameAmount('hi', /^hi/g,/hi$/g));
