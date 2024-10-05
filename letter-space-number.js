e3410ebb703b443116a690a5f7b07d540e7e4d6c
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-05 22:32:04 +0100

    letter-space-number.js

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,2 +1,3 @@
-const letterSpaceNumber = str => str.match(/([a-zA-Z]\s[0-9])(?=[^a-zA-Z0-9])/g)
+const letterSpaceNumber = str => str.match(/([a-zA-Z]\s[0-9])(?=[^a-zA-Z0-9])/g) || []
 // console.log(letterSpaceNumber('example 1, example 20'));
+// console.log(letterSpaceNumber('I like 7up.'));
