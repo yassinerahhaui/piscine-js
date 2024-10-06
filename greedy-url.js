a50faf840383eb6c5c7b8be38342dc9e92735538
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-06 04:06:50 +0100

    greedy-url.js

--- greedy-url.js
+++ greedy-url.js
@@ -1,4 +1,4 @@
-const dataSet = "qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you&hi=jjjj qhttp://example.com/hello?you=something&something=you qhttp://example.com/hello?you=something&something=you&hi=jjjj&bb=nnnnn"
+// const dataSet = "qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you&hi=jjjj qhttp://example.com/hello?you=something&something=you qhttp://example.com/hello?you=something&something=you&hi=jjjj&bb=nnnnn"
 const getURL = data => {
     let regex = /https?:\/\/(www.)?\w+.\w+/g
     return data.match(regex)
