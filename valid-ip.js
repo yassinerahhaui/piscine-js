66af196d6a319155303b70f5ffd27679c79387a1
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-06 06:18:57 +0100

    valid-ip.js

--- valid-ip.js
+++ valid-ip.js
@@ -1,5 +1,22 @@
-// const findIP = (str) => str.match(/[0-255]\.[0-255]\.[0-255]\.[0-255](:[10-65535])?/g) || []
 const findIP = (str) => {
-    const ipRegex = /^(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|[1-9])\.(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|[1-9])\.(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|[1-9])\.(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|[1-9]))(:[10-65535])?$/;
-    return str.match(ipRegex);
-  };
+    const ipPortRegex = /(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}(:([0-9]{1,5}))?/g;
+
+    const result = [];
+    let match;
+
+    while ((match = ipPortRegex.exec(str)) !== null) {
+        const ipWithPort = match[0];
+        const port = match[5];
+
+        if (port) {
+            const portNum = parseInt(port, 10);
+            if (portNum >= 0 && portNum <= 65535) {
+                result.push(ipWithPort);
+            }
+        } else {
+            result.push(ipWithPort);
+        }
+    }
+
+    return result.filter(ip => !/^255\.255\.255\.0$/.test(ip));
+}
