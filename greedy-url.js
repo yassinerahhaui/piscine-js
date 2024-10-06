ec99a025af80b5102fc26fa7de11d37c529a7804
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-06 04:06:13 +0100

    greedy-url.js

---
+++ greedy-url.js
@@ -0,0 +1,17 @@
+const dataSet = "qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you&hi=jjjj qhttp://example.com/hello?you=something&something=you qhttp://example.com/hello?you=something&something=you&hi=jjjj&bb=nnnnn"
+const getURL = data => {
+    let regex = /https?:\/\/(www.)?\w+.\w+/g
+    return data.match(regex)
+}
+const greedyQuery = data => {
+    let regex = /https?:\/\/(www.)?\w+.\w+\/\w+\?(\w+=\w+)&(\w+=\w+)(&(\w+=\w+))+/g
+    return data.match(regex)
+}
+const notSoGreedy = data => {
+    let regex = /https?:\/\/(www.)?\w+.\w+\/\w+\?(\w+=\w+)&(\w+=\w+)(&(\w+=\w+))?(?=(&(\w+=\w+)))/g
+    return data.match(regex)
+}
+
+// console.log(getURL(dataSet));
+// console.log(greedyQuery(dataSet));
+// console.log(notSoGreedy(dataSet));
