4567f6a405924b236e0233169f1ff2f61b978377
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-06 05:02:02 +0100

    greedy-url.js

--- greedy-url.js
+++ greedy-url.js
@@ -1,15 +1,23 @@
 // const dataSet = "qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello/yassine?you=something&something=you&hi=jjjj qhttp://example.com/hello?you=something&something=you qhttp://example.com/hello?you=something&something=you&hi=jjjj&bb=nnnnn"
-const getURL = data => {
-    let regex = /https?:\/\/(www.)?\w+.\w+((\/\w+)*(\?(\w+=\w+)+(&\w+=\w+)*)?)/g
-    return data.match(regex)
-}
+const regex = /https?:\/\/[^\s]+/g;
+const getURL = data => data.match(regex)
 const greedyQuery = data => {
-    let regex = /https?:\/\/(www.)?\w+.\w+\/\w+\?(\w+=\w+)&(\w+=\w+)(&(\w+=\w+))+/g
-    return data.match(regex)
+    let list = data.match(regex)
+    let res = []
+    for (let item of list) {
+        let arr = item.split("&")
+        if (arr.length >= 3) res = [...res,item]
+    }
+    return res
 }
 const notSoGreedy = data => {
-    let regex = /https?:\/\/(www.)?\w+.\w+\/\w+\?(\w+=\w+)&(\w+=\w+)(&(\w+=\w+))?(?=(&(\w+=\w+)))/g
-    return data.match(regex)
+    let list = data.match(regex)
+    let res = []
+    for (let item of list) {
+        let arr = item.split("&")
+        if (arr.length <= 3 && arr.length >= 2) res = [...res,item]
+    }
+    return res
 }
 
 // console.log(getURL(dataSet));
