9a17589c9883835fd91586e76937daf018a52902
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-06 05:33:05 +0100

    group-price.js

--- group-price.js
+++ group-price.js
@@ -2,7 +2,13 @@ const groupPrice = (str) => {
     let prices = str.match(/([A-Z]+|\$)[\d]+.[\d]+/g)
     let result = []
     for (let p of prices) {
-        let item = [...p.match(/([A-Z]+|\$)/g),...p.match(/\d+\.\d+/g)[0].split(".")]
+        let price = ""
+        if (p.match(/(\$)/g)) {
+            price = p
+        } else {
+            price = p.match(/([A-Z]+)/g)
+        }
+        let item = [price,...p.match(/\d+\.\d+/g)[0].split(".")]
         result = [...result,item]
     }
     return result
