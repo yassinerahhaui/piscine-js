2b7e9848b6d8f3383d0a05864f8e472938509f55
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-07 20:14:14 +0100

    dr-strange.js

---
+++ dr-strange.js
@@ -0,0 +1,36 @@
+const addWeek = (date) => {
+    let currentTime = new Date("0001-01-01").getTime()
+    let time = date.getTime() - currentTime
+    const day = (time / 1000 / 60 / 60 / 24 ) % 14
+    const weeks = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday',
+        'secondMonday', 
+        'secondTuesday',
+        'secondWednesday',
+        'secondThursday',
+        'secondFriday',
+        'secondSaturday',
+        'secondSunday'
+    ]
+    return weeks[day];
+}
+const timeTravel = (date) => {
+    Date.parse("Fri May 29 2020 21:22:22 GMT+0100 (Western European Summer Time)")
+    return date.date
+}
+// const time = Date.now() / 1000 / 60 / 60 / 24 / 365
+
+
+// console.log(addWeek(new Date("2024-10-07")));
+// console.log(timeTravel({
+//     date: new Date('2020-05-12 23:25:22'),
+//     hour: 21,
+//     minute: 22,
+//     second: 22,
+//   }).toString());
