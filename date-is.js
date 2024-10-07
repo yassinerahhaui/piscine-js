c8e6f92cccaf949c8acd22e97a55e9aba23f8b30
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-07 22:04:50 +0100

    date-is.js

--- date-is.js
+++ date-is.js
@@ -1,6 +1,11 @@
-const isValid = (date) => new Date(date).getTime() ? true : false
+const isValid = (date) => {
+    if (typeof date === "string") {
+        return false
+    }
+    return new Date(date).getTime() ? true : false
+}
 const isAfter = (date1,date2) => (date1.getTime() > date2.getTime())
 const isBefore = (date1, date2) => (date1.getTime() < date2.getTime())
 const isFuture = (date) => (date.getTime() > Date.now().getTime())
 const isPast = (date) => (date.getTime() < Date.now().getTime())
-// console.log(isValid(new Date()));
+// console.log(isValid(1393804800000));
