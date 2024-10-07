946885f080db2d18e1bd4093df93b85fc80cd332
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-07 22:28:31 +0100

    date-is.js

--- date-is.js
+++ date-is.js
@@ -4,8 +4,26 @@ const isValid = (date) => {
     }
     return new Date(date).getTime() ? true : false
 }
-const isAfter = (date1,date2) => (date1.getTime() > date2.getTime())
-const isBefore = (date1, date2) => (date1.getTime() < date2.getTime())
-const isFuture = (date) => (date.getTime() > Date.now().getTime())
-const isPast = (date) => (date.getTime() < Date.now().getTime())
+const isAfter = (arg1,arg2) => {
+    if (typeof arg1 === "string" || typeof arg2 === "string") return false;
+    let date1 = new Date(arg1).getTime()
+    let date2 = new Date(arg2).getTime()
+    return (date1.getTime() > date2.getTime())
+}
+const isBefore = (arg1, arg2) => {
+    if (typeof arg1 === "string" || typeof arg2 === "string") return false;
+    let date1 = new Date(arg1).getTime()
+    let date2 = new Date(arg2).getTime()
+    return (date1.getTime() < date2.getTime())
+}
+const isFuture = (date) => {
+    if (typeof date === "string") return false;
+    let date = new Date(date).getTime()
+    return (date.getTime() > Date.now().getTime())
+}
+const isPast = (date) => {
+    if (typeof date === "string") return false;
+    let date = new Date(date).getTime()
+    return (date.getTime() < Date.now().getTime())
+}
 // console.log(isValid(1393804800000));
