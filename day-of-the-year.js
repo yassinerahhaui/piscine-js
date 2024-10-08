575b57bcdb0c774839deb9c5ef2c9668b31a96e5
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-08 14:07:13 +0100

    day-of-the-year.js

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,9 @@
 const dayOfTheYear = (date) => {
     let month = date.getMonth()
-    let days = month * 30 + (month/2)
+    let days = month * 30
     days += date.getDate()
-    return days
+    if (month % 2 === 1) month++;
+    return days + (month/2)
 }
+
+// console.log(dayOfTheYear(new Date('1664-08-09')));
