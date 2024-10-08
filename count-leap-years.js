01a4f20d6ada6727a1ba585053b524c7d56ebc8d
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-08 13:44:25 +0100

    count-leap-years.js

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,8 +1,9 @@
 const countLeapYears = (date) => {
-    let years = date.getFullYear()
+    let year = date.getFullYear()
     let res = 0
-    for (let i = 1; i <= years; i++) {
-        if (i%4 === 0) res++;
+    for (let i = 1; i < year; i++) {
+        if (i%4 === 0 && (i%100 !== 0 || i%400 === 0)) res++;
     }
     return res
 }
+console.log(countLeapYears(new Date('1664-08-09')));
