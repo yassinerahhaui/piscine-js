c4eab25b9afde899bb810bbf55850c25bc6f343d
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-08 15:53:32 +0100

    day-of-the-year.js

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -2,7 +2,6 @@ const dayOfTheYear = (date) => {
     let startDay = date.getDate()
     let startMonth = date.getMonth()
     let startYear = date.getFullYear()
-    console.log(startYear);
     if (startDay === 0 && startMonth === 0 && startYear === 1) return 1;
     let month = date.getMonth()
     let day = date.getDate()
