d4134332ad0b67683cc970cdc10bc4faee33a2e5
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-08 10:50:39 +0100

    get-some-time.js

--- get-some-time.js
+++ get-some-time.js
@@ -17,6 +17,7 @@ const formatDate = (dd, mm , yyyy) => {
     mm = Number(mm+1)
     let month = mm.toString().padStart(2,'0')
     let day = dd.toString().padStart(2, '0')
-    return `${day}-${month}-${yyyy}`
+    let year = yyyy.toString().padStart(4,'0')
+    return `${day}-${month}-${year}`
 }
-// console.log(firstDayWeek(1,"1000"));
+// console.log(firstDayWeek(1,"0001"));
