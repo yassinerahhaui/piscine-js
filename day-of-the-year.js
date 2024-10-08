19aca2bef3b48d62d22d04d5eaed2c7efbf2da02
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-08 16:19:19 +0100

    day-of-the-year.js

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,14 +1,13 @@
 const dayOfTheYear = (date) => {    
     let month = date.getMonth()
     let day = date.getDate()
-    let res = 0
+    let res = day
     let daysofMonths = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30]
     let year = date.getFullYear()
     if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) daysofMonths[1] = 29;
     for (let i = 0; i < month; i++) {
         res += daysofMonths[i]
     }
-    res += day
     return res
 }
 
