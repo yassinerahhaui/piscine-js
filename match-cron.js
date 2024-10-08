edefbdf3fa0c1238dfca0c9c74c2d05bb1e0b0b2
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-08 13:16:10 +0100

    match-cron.js

--- match-cron.js
+++ match-cron.js
@@ -10,7 +10,7 @@ const matchCron = (cron, date) => {
 
     if (cron[0] !== '*' && Number(cron[0]) !== dt.min) return false;
     if (cron[1] !== '*' && Number(cron[1]) !== dt.hour) return false;
-    if (cron[2] !== '*' && Number(cron[2]) !== dt.dayOfMonth+1) return false;
+    if (cron[2] !== '*' && Number(cron[2]) !== dt.dayOfMonth) return false;
     if (cron[3] !== '*' && Number(cron[3]) !== dt.month+1) return false;
     if (dt.dayOfWeek === 0 && cron[0] === '7') {
 
@@ -20,7 +20,7 @@ const matchCron = (cron, date) => {
 
     return true
 }
-
+// console.log(matchCron('* * 9 * *', new Date('2020-06-09 00:00:00')));
 // console.log(matchCron('9 * * * *', new Date('2020-05-30 18:09:00')));
 // console.log(matchCron('9 * * * *', new Date('2020-05-30 19:09:00')));
 // console.log(matchCron('9 * * * *', new Date('2020-05-30 19:21:00')));
