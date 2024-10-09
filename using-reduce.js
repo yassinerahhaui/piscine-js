4e5afdd966f0a2f9d9ca0fc1b41aaae41639e9fe
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-09 23:28:48 +0100

    using-reduce.js

--- using-reduce.js
+++ using-reduce.js
@@ -1,6 +1,10 @@
-const adder = (arr) => arr.reduce((total,current)=> total + current)
-const sumOrMul = (arr)=> arr.reduce((total,num)=> num%2 === 0 ? total * num : total + num)
-const funcExec = (arr) => arr.reduce((_, func)=> func())
+const adder = (arr,initial) => arr.reduce((total,current)=> {
+    return total + current
+},initial)
+const sumOrMul = (arr,initial)=> arr.reduce((total,num)=> {
+    return num%2 === 0 ? total * num : total + num
+},initial)
+const funcExec = (arr,initial) => arr.reduce((total, func)=> func(total),initial)
 
 // let arr = [1,2,3,4,5,6,7,8,9]
 
