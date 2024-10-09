10ad79c7703397d3d11e809e16d5151e37d44041
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-09 19:17:41 +0100

    using-filter.js

--- using-filter.js
+++ using-filter.js
@@ -16,19 +16,29 @@ const filter1DistinctVowel = (arr) => arr.filter(el => {
     return true
 })
 
-const multiFilter = (arr) => arr.filter(el => {
-    return (el.capital.length >= 8 && /^[^aeiou]/gi.test(el.name) && /[^aeiou]$/gi.test(el.tag) && el.region != "South")
-})
-// let ar = [
-//     'Alabama',
-//     'Alaska',
-//     'Arkansas',
-//     'Kansas',
-//     'Maryland',
-//     'Mississippi',
-//     'New Jersey',
-//     'Tennessee',
-//   ]
+const multiFilter = (arr) => arr.filter(el => (el.capital.length >= 8 && /^[^aeiou]/gi.test(el.name) && /[^aeiou]$/gi.test(el.tag) && el.region !== "South"))
+let ar = [
+    { tag: 'CA', name: 'California', capital: 'Sacramento', region: 'West' },
+    { tag: 'HI', name: 'Hawaii', capital: 'Honolulu', region: 'West' },
+    {
+      tag: 'MO',
+      name: 'Missouri',
+      capital: 'Jefferson City',
+      region: 'Midwest',
+    },
+    {
+      tag: 'PA',
+      name: 'Pennsylvania',
+      capital: 'Harrisburg',
+      region: 'Northeast',
+    },
+    {
+      tag: 'RI',
+      name: 'Rhode Island',
+      capital: 'Providence',
+      region: 'Northeast',
+    },
+  ]
 
-// console.log(filter1DistinctVowel(ar));
+console.log(multiFilter(ar));
 // console.log(strREg.test(str));
