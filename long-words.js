67220e744f37434e95d86537ef658f693471eaa5
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-10 02:02:57 +0100

    long-words.js

--- long-words.js
+++ long-words.js
@@ -1,4 +1,10 @@
-const longWords = (arr = []) => arr.map(el=> {
+const longWords = (arr) => arr.every((el)=> (typeof el === 'string' && el.length >= 5))
+
+const oneLongWord = (arr) => arr.some((el)=> (typeof el === 'string' && el.length >= 10))
+
+const noLongWords = (arr) => !arr.some((el)=> (typeof el === "string" && el.length >= 7))
+
+/* const longWords = (arr = []) => arr.map(el=> {
     return (typeof el === 'string' && el.length >= 5)
 }).reduce((total,cur)=> {
     if (!cur || !total) total = false
@@ -15,4 +21,6 @@ const oneLongWord = (arr = []) => arr.reduce((total,el)=> {
 const noLongWords = (arr = []) => arr.reduce((total,el)=> {
     if (typeof el === "string" && el.length >= 7) total = false
     return total
-},true)
+},true) */
+// let arr = ["12345",10,"1231","12345"]
+// console.log(noLongWords(arr));
