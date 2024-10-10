3de6329308829b4a83bb857225ac63105356509e
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-10 05:30:13 +0100

    reduce.js

--- reduce.js
+++ reduce.js
@@ -7,21 +7,24 @@ const fold = (arr = [], func, init = 0) => {
 }
 const foldRight = (arr = [], func, init = 0) => {
     let total = init
-    for (let i = arr.length-1; i >0 ; i--) {
+    let start = arr.length - 2
+    init === 0 ? total = arr[0] : start = arr.length - 2
+    for (let i = start; i > 0; i--) {
         total = func(total, arr[i], i, arr)
     }
     return total
 }
-const reduce = (arr=[], func) => {
+
+const reduce = (arr = [], func) => {
     let total = arr[0] || []
     for (let i = 1; i < arr.length; i++) {
         total = func(total, arr[i], i, arr)
     }
     return total
 }
-const reduceRight = (arr=[], func) => {
-    let total = arr[arr.length-1] || []
-    for (let i = arr.length-2; i > 0; i--) {
+const reduceRight = (arr = [], func) => {
+    let total = arr[arr.length - 1] || []
+    for (let i = arr.length - 2; i > 0; i--) {
         total = func(total, arr[i], i, arr)
     }
     return total
