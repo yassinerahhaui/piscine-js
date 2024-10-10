e86edc01393564398d3736903d9f4a53a103c109
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-10 02:46:13 +0100

    mapper.js

--- mapper.js
+++ mapper.js
@@ -17,6 +17,9 @@ const flat = (arr, num=1) => {
 }
 // const arr = [1,2,3,4]
 // console.log(map(arr, (i)=> i+1));
-const flatMap = (arr, func) => map(flat(arr),func)
+const flatMap = (arr, func) => {
+    let newarr = flat(arr)
+    return map(newarr, func)
+}
 
-// console.log(flatMap([[1,2],[3,4],[5,6]], (i) => i+1));
+console.log(flatMap([[1,2],[3,4],[5,6]], (i) => i+1));
