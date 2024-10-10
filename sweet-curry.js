6b12406cf4d4f02d8bcb56d4d7659f9de2b47871
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-10 07:02:03 +0100

    sweet-curry.js

---
+++ sweet-curry.js
@@ -0,0 +1,7 @@
+const mult2 = (a) => (b) => a * b;
+const add3 = (a) => (b) => (c) => a + b + c;
+const sub4 = (a) => (b) => (c) => (d) => a - b - c - d;
+
+// console.log(mult2(2)(3));
+// console.log(add3(2)(3)(5));
+// console.log(sub4(120)(3)(2)(2));
