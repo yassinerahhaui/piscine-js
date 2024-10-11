d6e36a5dc0346036914f6e4b64e88013e9db24a7
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-11 02:02:12 +0100

    flow.js

--- flow.js
+++ flow.js
@@ -1,12 +1,16 @@
-
-
-// const square = (nbr) => nbr * nbr
-// const add2Numbers = (nbr1, nbr2) => nbr1 + nbr2
 const flow = (funcs) => {
     return (...args) => {
-        const res = funcs[0](...args)
-        return funcs[1](res)
+        return funcs.reduce((result, func) => {
+            result = Array.isArray(result) ? func(...result) : func(result)
+            return result
+        }, args)
     }
+
 }
-// const flowedFunctions = flow([add2Numbers, square])
-// console.log(flowedFunctions(2, 3) )// -> 25
+
+/*
+const square = (nbr) => nbr * nbr
+const add2Numbers = (nbr1, nbr2) => nbr1 + nbr2
+const flowedFunctions = flow([add2Numbers, square])
+console.log(flowedFunctions(2, 3))// -> 25
+*/
