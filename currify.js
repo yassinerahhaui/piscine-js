475f173f923efaa368b3d12f72c89211d93aff1d
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-11 04:55:10 +0100

    currify.js

--- currify.js
+++ currify.js
@@ -1,12 +1,1 @@
-const currify = (func) => {
-    return (arg1) => (arg2) => {
-        return func(arg1 * arg2)
-    }
-}
-// const mult2 = (el1, el2) => el1 * el2
-// // console.log(mult2(2, 2)) // result expected 4
-
-// const mult2Curried = currify(mult2)
-
-// console.log(mult2Curried(2)(2)) // result expected 4
-// // (same result, with a function that has technically only one argument)
+const currify = func => (...args) => args.length >= func.length ? func(...args) : (...more) => currify(func)(...args, ...more)
