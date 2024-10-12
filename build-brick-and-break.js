5d789caa1b9ee5a2ff4bffeb4f502a86d7b67de8
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-12 19:56:43 +0100

    build-brick-and-break.js

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -2,7 +2,7 @@ const build = (maximum) => {
     const body = document.body
     const setBuild = setInterval(()=> {
         const div = document.createElement('div');
-        if (maximum % 3 == 2) {
+        if (maximum % 3 === 2) {
             div.dataset.foundation = 'true'
         }
         div.classList.add(`brick-${maximum}`);
@@ -13,25 +13,18 @@ const build = (maximum) => {
     },100)
 }
 const repair = (...ids) => {
-    // let els = Array.from(document.querySelectorAll("[class^='brick-']"))
     ids.forEach(id => {
         const el = document.querySelector(`.${id}`)
-        let num = id.split('-')[1]
-        if (el.getAttribute("foundation")) {
+        if (el.getAttribute("data-foundation")) {
             el.dataset.repaired = "in progress"
-            // el.textContent = "in progress"
-            // el.style.color = "black"
         } else {
             el.dataset.repaired = "true"
-            // el.textContent = `${num}-repaired`
-            // el.style.color = "blue"
         }
     })
 }
 const destroy = () => {
     let last = Array.from(document.querySelectorAll("[class^='brick-']"))
-    last = last[last.length-1]
-    document.body.removeChild(last)
+    last[last.length-1].remove()
 }
 
 export {build, repair,destroy}
