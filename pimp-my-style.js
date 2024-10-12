6e490b7ff610a21c71e71f413446a2953eb30062
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-13 00:55:33 +0100

    pimp-my-style.js

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,12 +1,20 @@
-import {styles} from "./pimp-my-style.data.js"
+import { styles } from "./pimp-my-style/pimp-my-style.data.js"
 let count = 0
 let plus = true
+let ln = styles.length-1
 export const pimp = () => {
-    let ln = styles.length
     const btn = document.querySelectorAll('button.button')
     btn[0].classList.toggle(styles[count])
-    console.log(btn[0].classList.value);
+    // console.log(btn[0].classList.value);
     plus ? count++ : count--
-    if (count >= ln) plus = false
-    if (count <= 0) plus = true
+    if (count >= ln) {
+        plus = false
+        btn[0].classList.toggle(styles[count])
+        // console.log(btn[0].classList.value);
+    };
+    if (count <= 0) {
+        plus = true
+        btn[0].classList.toggle(styles[count])
+        // console.log(btn[0].classList.value);
+    }
 }
