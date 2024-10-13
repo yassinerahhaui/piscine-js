eddad589315c98202fb9a90ddf6f74cb5afe2053
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-13 03:06:09 +0100

    fifty-shades-of-cold.js

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -2,12 +2,9 @@
 import { colors } from "./fifty-shades-of-cold.data.js"
 const generateClasses = () => {
     let style = document.createElement('style')
-    let textStyle = ""
     colors.map(color=> {
-        let item = `.${color} {\n\t background-color: ${color};\n}\n`
-        textStyle += item
+        style.innerText `.${color} {\n\tbackground: ${color};\n}\n`
     })
-    style.innerText = textStyle
     document.head.appendChild(style)
 }
 
@@ -15,7 +12,6 @@ const generateColdShades = () => {
     colors.map(color => {
         let div = document.createElement('div')
         div.className = color
-        // div.style.backgroundColor = color
         div.textContent = color
         document.body.appendChild(div)
     })
@@ -24,7 +20,7 @@ const generateColdShades = () => {
 const choseShade = (e) => {
     const divs = Array.from(document.getElementsByTagName("div"))
     divs.forEach(div => {
-        div.style.backgroundColor = e
+        div.style.background = e
     });
 }
 
