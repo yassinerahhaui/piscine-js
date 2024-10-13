668eeb3f3fc7a176bd35acc9915092edea5dddb3
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-13 04:39:32 +0100

    keycodes-symphony.js

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -1,16 +1,18 @@
 export const compose = () => {
     window.addEventListener("keydown", (e) => {
         const { key } = e
-        console.log(key);
         if (key === "Backspace" || key === "Delete") {
             let divs = Array.from(document.querySelectorAll('.note'))
-            document.body.removeChild(divs[divs.length-1])
+            document.body.removeChild(divs[divs.length - 1])
         } else {
-            const div = document.createElement("div")
-            div.className = "note"
-            div.style.background = `hsl(${Math.floor(Math.random() * 360)},100%,50%)`
-            div.textContent = `${key}`
-            document.body.appendChild(div)
+
+            if (key.match(/[a-z]/g) !== null) {
+                const div = document.createElement("div")
+                div.className = "note"
+                div.style.background = `hsl(${Math.floor(Math.random() * 360)},100%,50%)`
+                div.textContent = `${key}`
+                document.body.appendChild(div)
+            }
         }
         if (key === "Escape") {
             document.body.innerHTML = ''
