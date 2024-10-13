c7ad6444f8d51e9adb7dae92bb46b9e0ab75517b
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-13 03:25:27 +0100

    fifty-shades-of-cold.js

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -3,7 +3,7 @@ import { colors } from "./fifty-shades-of-cold.data.js"
 
 const generateClasses = () => {
     let style = document.createElement('style')
-    colors.map(color => {
+    colors.forEach(color => {
         style.innerHTML += `.${color} {\n\tbackground: ${color};\n}`
     })
     document.head.appendChild(style)
