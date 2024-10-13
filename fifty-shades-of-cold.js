2e5b2dbcd43971ead0e036528a2241ad959d8fce
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-13 02:57:35 +0100

    fifty-shades-of-cold.

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,30 @@
+import { colors } from "./fifty-shades-of-cold.data.js"
+const generateClasses = () => {
+    let style = document.createElement('style')
+    let textStyle = ""
+    colors.map(color=> {
+        let item = `.${color}:{\n\t background-color: ${color} !important;\n}\n`
+        textStyle += item
+    })
+    style.innerHTML = textStyle
+    document.head.appendChild(style)
+}
+
+const generateColdShades = () => {
+    colors.map(color => {
+        let div = document.createElement('div')
+        div.className = color
+        div.style.backgroundColor = color
+        div.textContent = color
+        document.body.appendChild(div)
+    })
+}
+
+const choseShade = (e) => {
+    const divs = Array.from(document.getElementsByTagName("div"))
+    divs.forEach(div => {
+        div.style.backgroundColor = e
+    });
+}
+
+export {generateClasses, generateColdShades, choseShade}
