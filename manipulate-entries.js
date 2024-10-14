abac016c89974dcc9022833f0b1ac536f59c3e6c
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-15 00:54:10 +0100

    manipulate-entries.js

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -27,9 +27,43 @@ const reduceEntries = (obj, func, init) => {
     return result
 }
 
-const totalCalories = () => {}
-const lowCarbs = () => {}
-const cartTotal = () => {}
+const nutritionDB = {
+    tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+    vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+    oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+    onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+    garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+    paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+    sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+    orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
+  }
+
+const totalCalories = (obj) => {
+    let result = 0
+    for (let [key, val] of Object.entries(obj)) {
+        result += nutritionDB[key].calories * val
+    }
+    return result / 100
+}
+const lowCarbs = (obj) => {
+    const result ={}
+    let minKey = Object.keys(obj)[0]
+    let minVal = nutritionDB[minKey].carbs * obj[minKey]
+    for (let [key, val] of Object.entries(obj)) {
+        const item = nutritionDB[key].carbs * val
+        if (minVal > item) {
+            minKey = key;
+            minVal = item
+        }
+    }
+    result[minKey] = obj[minKey]
+    return result
+}
+const cartTotal = (obj) => {
+    const result = {}
+    // for ()
+
+}
 
 
 /* tests */
