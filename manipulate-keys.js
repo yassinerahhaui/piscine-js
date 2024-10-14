daeab9c174a4a594737c6e64ca24b092514f3fc7
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-14 18:40:03 +0100

    manipulate-keys.js

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -19,7 +19,7 @@ const reduceKeys = (obj, func, init) => {
     let result = init || Object.keys(obj)[0]
     for (let key of Object.keys(obj)){
         if (!init && key === Object.keys(obj)[0]) continue;
-        result = func(result, key, obj[key], obj)
+        result = func(result, key, obj)
     }
     return result
 }
