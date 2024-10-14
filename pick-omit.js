91873ffb1829277a5b4935826afc039ad8be5347
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-14 14:17:13 +0100

    pick-omit.js

---
+++ pick-omit.js
@@ -0,0 +1,26 @@
+const pick = (obj,arr) => {
+    const res = {}
+    if (typeof arr === 'string') {
+        if (obj[arr] !== undefined) {
+            res[arr] = obj[arr]
+        }
+    } else {
+        arr.forEach(str => {
+            if (obj[str] !== undefined) {
+                res[str] = obj[str]
+            }
+        });
+    }
+}
+const omit = (obj,arr) => {
+    const res = {}
+    for (let [key,val] of Object.entries(obj)) {
+        if (typeof arr === 'string') {
+            if (key !== arr) {
+                res[key] = val
+            }
+        } else {
+            if (!arr.includes(key)) res[key] = val;
+        }
+    }
+}
