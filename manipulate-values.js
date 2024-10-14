a8289a669771118aacc6bda4f12012f7e98531be
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-14 17:23:03 +0100

    manipulate-values.js

--- manipulate-values.js
+++ manipulate-values.js
@@ -14,20 +14,20 @@ const mapValues = (obj, func) => {
     }
     return result
 }
-const reduceValues = (obj, func) => {
-    if (obj.length < 1 || obj === undefined) {
-        throw new Error('Object error: Cannot reduce an empty Object!');
-    }
-    let firstkey = Object.keys(obj)[0]
-    let result = obj[firstkey]
+const reduceValues = (obj, func, init = Object.values(obj)[0]) => {
+    // if (obj.length < 1 || obj === undefined) {
+    //     throw new Error('Object error: Cannot reduce an empty Object!');
+    // }
+    // // let firstkey = Object.keys(obj)[0]
+    let result = init
     for (let [key, val] of Object.entries(obj)) {
-        if (key !== firstkey) {
+        if (init !== val) {
             result = func(result, val, key, obj)
         }
     }
     return result
 }
 
-// const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
 
-// console.log(reduceValues(nutrients, (acc, cr) => acc + cr))
+console.log(reduceValues(nutrients, (acc, cr) => acc + cr))
