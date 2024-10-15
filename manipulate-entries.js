2a87134eab1e7daf4fb6904e0631169e4bbbb5e9
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-15 16:33:29 +0100

    manipulate-entries.js

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -10,7 +10,7 @@ const filterEntries = (obj, func) => {
 const mapEntries = (obj, func) => {
     const result = {}
     for (let [key, val] of Object.entries(obj)) {
-        result[key] = func([key, val])
+        result[key] = func(obj[key],[key, val],obj)
     }
     return result
 }
@@ -28,16 +28,16 @@ const reduceEntries = (obj, func, init) => {
     return result
 }
 
-// const nutritionDB = {
-//     tomato: { calories: 18, protein: 0.9, carbs: 3.9, sugar: 2.6, fiber: 1.2, fat: 0.2 },
-//     vinegar: { calories: 20, protein: 0.04, carbs: 0.6, sugar: 0.4, fiber: 0, fat: 0 },
-//     oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
-//     onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
-//     garlic: { calories: 149, protein: 6.4, carbs: 33, sugar: 1, fiber: 2.1, fat: 0.5 },
-//     paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1, fiber: 0, fat: 12.89 },
-//     sugar: { calories: 387, protein: 0, carbs: 100, sugar: 100, fiber: 0, fat: 0 },
-//     orange: { calories: 49, protein: 0.9, carbs: 13, sugar: 9, fiber: 0.2, fat: 0.1 },
-// }
+const nutritionDB = {
+    tomato: { calories: 18, protein: 0.9, carbs: 3.9, sugar: 2.6, fiber: 1.2, fat: 0.2 },
+    vinegar: { calories: 20, protein: 0.04, carbs: 0.6, sugar: 0.4, fiber: 0, fat: 0 },
+    oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
+    onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
+    garlic: { calories: 149, protein: 6.4, carbs: 33, sugar: 1, fiber: 2.1, fat: 0.5 },
+    paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1, fiber: 0, fat: 12.89 },
+    sugar: { calories: 387, protein: 0, carbs: 100, sugar: 100, fiber: 0, fat: 0 },
+    orange: { calories: 49, protein: 0.9, carbs: 13, sugar: 9, fiber: 0.2, fat: 0.1 },
+}
 
 const totalCalories = (obj) => {
     let result = 0
@@ -77,12 +77,37 @@ const cartTotal = (obj) => {
 }
 
 
-// /* tests */
-// const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
+/* tests */
+const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
 
-// console.log('Total calories:')
-// console.log(totalCalories(groceriesCart))
-// console.log('Items with low carbs:')
-// console.log(lowCarbs(groceriesCart))
-// console.log('Total cart nutritional facts:')
-// console.log(cartTotal(groceriesCart))
+console.log('Total calories:')
+console.log(totalCalories(groceriesCart))
+console.log('Items with low carbs:')
+console.log(lowCarbs(groceriesCart))
+console.log('Total cart nutritional facts:')
+console.log(cartTotal(groceriesCart))
+// -   '✔️oil': 250,
+// -   '✔️paprika': 230,
+// -   '❌garlic': -30,
+// -   '❌onion': -20
+//   }
+
+//   + actual - expected
+
+//   {
+// +   garlic: [
+// +     '❌garlic',
+// +     -30
+// +   ],
+// +   oil: [
+// +     '✔️oil',
+// +     250
+// +   ],
+// +   onion: [
+// +     '❌onion',
+// +     -20
+// +   ],
+// +   paprika: [
+// +     '✔️paprika',
+// +     230
+// +   ]
