959880a90932ec5c158363a5adb6496f4ba3d3f1
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-15 16:20:04 +0100

    manipulate-entries.js

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,7 +1,7 @@
 const filterEntries = (obj, func) => {
     const res = {}
-    for (let [key,val] of Object.entries(obj)) {
-        if (func([key, val],key, obj)) {
+    for (let [key, val] of Object.entries(obj)) {
+        if (func([key, val])) {
             res[key] = val
         }
     }
@@ -10,33 +10,34 @@ const filterEntries = (obj, func) => {
 const mapEntries = (obj, func) => {
     const result = {}
     for (let [key, val] of Object.entries(obj)) {
-        result[key] = func([key, val], key, obj)
+        result[key] = func([key, val])
     }
     return result
 }
 
 const reduceEntries = (obj, func, init) => {
-    let [firstkey,firstVal] = Object.entries(obj)[0]
-    let result = init || [firstkey,firstVal]
-    for (let [key, val] of Object.entries(obj)) {
-        if (!init && key === firstkey) {
-            continue
-        }
-        result = [func(result, val, key, obj), func(result, key, key, obj)]
+    let result = init
+    const arr = Object.entries(obj)
+    if (typeof init === 'undefined') {
+        result = arr[0]
+        arr = arr.slice(1)
+    }
+    for (let item of arr) {
+        result = func(result, [item[0], item[1]])
     }
     return result
 }
 
 const nutritionDB = {
-    tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
-    vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
-    oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
-    onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
-    garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
-    paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
-    sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
-    orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
-  }
+    tomato: { calories: 18, protein: 0.9, carbs: 3.9, sugar: 2.6, fiber: 1.2, fat: 0.2 },
+    vinegar: { calories: 20, protein: 0.04, carbs: 0.6, sugar: 0.4, fiber: 0, fat: 0 },
+    oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
+    onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
+    garlic: { calories: 149, protein: 6.4, carbs: 33, sugar: 1, fiber: 2.1, fat: 0.5 },
+    paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1, fiber: 0, fat: 12.89 },
+    sugar: { calories: 387, protein: 0, carbs: 100, sugar: 100, fiber: 0, fat: 0 },
+    orange: { calories: 49, protein: 0.9, carbs: 13, sugar: 9, fiber: 0.2, fat: 0.1 },
+}
 
 const totalCalories = (obj) => {
     let result = 0
@@ -45,10 +46,12 @@ const totalCalories = (obj) => {
     }
     return result / 100
 }
+
 const lowCarbs = (obj) => {
-    const result ={}
+    const result = {}
     let minKey = Object.keys(obj)[0]
     let minVal = nutritionDB[minKey].carbs * obj[minKey]
+
     for (let [key, val] of Object.entries(obj)) {
         const item = nutritionDB[key].carbs * val
         if (minVal > item) {
@@ -59,19 +62,27 @@ const lowCarbs = (obj) => {
     result[minKey] = obj[minKey]
     return result
 }
+/* orange: { calories: 245, protein: 4.5, carbs: 65, sugar: 45, fiber: 1, fat: 0.5 } */
 const cartTotal = (obj) => {
+    // { orange: 500, oil: 20, sugar: 480 }
     const result = {}
-    // for ()
-
+    for (let [key, value] of Object.entries(obj)) {
+        const child = {}
+        for (let [k,v] of Object.entries(nutritionDB[key])) {
+            child[k] = parseFloat((v * value) /100)
+        }
+        result[key] = child
+    }
+    return result
 }
 
 
-/* tests */
-const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
+// /* tests */
+// const groceriesCart = { orange: 500, oil: 20, sugar: 480 }
 
-console.log('Total calories:')
-console.log(totalCalories(groceriesCart))
-console.log('Items with low carbs:')
-console.log(lowCarbs(groceriesCart))
-console.log('Total cart nutritional facts:')
-console.log(cartTotal(groceriesCart))
+// console.log('Total calories:')
+// console.log(totalCalories(groceriesCart))
+// console.log('Items with low carbs:')
+// console.log(lowCarbs(groceriesCart))
+// console.log('Total cart nutritional facts:')
+// console.log(cartTotal(groceriesCart))
