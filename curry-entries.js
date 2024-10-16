a047474f73aa8254a8073547d84f1e89551559fb
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-16 13:37:30 +0100

    create defaultCurry function in curry-entries.js

--- curry-entries.js
+++ curry-entries.js
@@ -1,7 +1,21 @@
-const defaultCurry = () => {}
+const defaultCurry = (arg1) => (arg2) => {
+    for (let [key,val] of Object.entries(arg2)) {
+        arg1[key] = val;
+    }
+    return arg1
+}
 const reduceCurry = () => {}
 const filterCurry = () => {}
 
 const reduceScore = () => {}
 const filterForce = () => {}
 const mapAverage = () => {}
+// console.log(defaultCurry({
+//     http: 403,
+//     connection: 'close',
+//     contentType: 'multipart/form-data',
+//   })({
+//     http: 200,
+//     connection: 'open',
+//     requestMethod: 'GET'
+//   }));
