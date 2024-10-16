a19826ddf998094bbe8a60bd776ffb929f4c6f07
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-16 23:34:02 +0100

    replica.js

--- replica.js
+++ replica.js
@@ -1,10 +1,39 @@
-const replica = (obj) => {
-    if (typeof obj !== "object" || typeof obj === null || typeof obj === 'function' || obj instanceof RegExp) {
-        return obj;
+// const replica = ( ...args) => {
+//     let result = args[1]
+//     for (let i = 1;i < Object.keys(args).length; i++) {
+//         for (let[key,val] of Object.entries(args[i])) {
+//             result[key] = val
+//         }
+//     }
+//     // console.log(...args);
+//     return result
+// }
+const replica = (...args) => {
+    const result = {};
+
+    for (let i = 1; i < args.length; i++) {
+        const current = args[i];
+        for (const [key, val] of Object.entries(current)) {
+            if (typeof val === 'object' && !Array.isArray(val) && val !== null) {
+                result[key] = result[key] || {};
+                Object.assign(result[key], val);
+            } else {
+                result[key] = val;
+            }
+        }
     }
-    const result = Array.isArray(obj) ? [] : {}
-    for (let [key,_] of Object.entries(obj)) {
-        result[key] = replica(Object.assign(result[key],obj[key]))
-    }
-    return result
+    Object.assign(result, args[0]);
+    
+    return result;
 }
+
+
+console.log(replica(
+    {},
+    { a: { b1: 1, c1: 2 } },
+    { a: { b1: { d2: 1, e2: 2 } } },
+    { a: { b1: { d2: { f3: 1, h3: 1 }, e2: { g3: 2 } } } },
+    { a: { b1: { d2: { f3: { i4: 1 }, h3: 1 }, e2: { g3: 2 } } } }
+));
+//  { a: { b1: { d2: { f3: { i4: 1 }, h3: 1 }, e2: { g3: 2 } }, c1: 2 } }
+console.log({ a: { b1: { d2: { f3: { i4: 1 }, h3: 1 }, e2: { g3: 2 } } } });
