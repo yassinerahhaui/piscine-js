e52b234d6a671a3c466e9d0ecf41dd78fe98a23b
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-16 22:33:40 +0100

    test

--- replica.js
+++ replica.js
@@ -1,21 +1,10 @@
-const deepCopy = (obj) => {
+const replica = (obj) => {
     if (typeof obj !== "object" || typeof obj === null || typeof obj === 'function' || obj instanceof RegExp) {
         return obj;
     }
     const result = Array.isArray(obj) ? [] : {}
     for (let [key,_] of Object.entries(obj)) {
-        result[key] = deepCopy(obj[key])
+        result[key] = replica(obj[key])
     }
     return result
 }
-
-// const obj = {
-//     a: 1,
-//     c: 15,
-//     arr: ["a","b"],
-//     child: {
-//         a: 1,
-//         b: 10
-//     }
-// }
-// console.log(deepCopy(obj));
