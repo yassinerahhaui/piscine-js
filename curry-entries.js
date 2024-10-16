06c5c7770ebab8b3caf957251daecd8561f03997
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-16 16:46:07 +0100

    curry-entries

--- curry-entries.js
+++ curry-entries.js
@@ -1,15 +1,58 @@
-const defaultCurry = (arg1) => (arg2) => {
-    for (let [key,val] of Object.entries(arg2)) {
-        arg1[key] = val;
+const defaultCurry = (obj1) => (obj2) => {
+    for (let [key,val] of Object.entries(obj2)) {
+        obj1[key] = val;
     }
-    return arg1
+    return obj1
 }
-const reduceCurry = () => {}
-const filterCurry = () => {}
+const mapCurry = (func) => (obj) => {
+    const result = {}
+    for (let [key,val] of Object.entries(obj)) {
+        [key,val] = func([key,val])
+        result[key] = val
+    }
+    return result
+}
+const reduceCurry = (func) => (obj,init) => {
+    let result = init
+    const arr = Object.entries(obj)
+    if (typeof init === 'undefined') {
+        result = arr[0]
+        arr = arr.slice(1)
+    }
+    for (let item of arr) {
+        result = func(result, [item[0], item[1]])
+    }
+    return result
+}
+const filterCurry = (func) => (obj) => {
+    const res = {}
+    for (let [key, val] of Object.entries(obj)) {
+        if (func([key, val])) {
+            res[key] = val
+        }
+    }
+    return res
+}
 
-const reduceScore = () => {}
-const filterForce = () => {}
-const mapAverage = () => {}
+// const personnel = {
+//     lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+//     sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+//     zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+//     ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+//     calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+//   }
+  
+
+const reduceScore = (personnel) => reduceCurry((person => {
+    if (person.isForceUser) {
+        return person.pilotingScore + person.shootingScore
+    }
+}))(personnel,0)
+const filterForce = (personnel) => filterCurry((person)=> person.shootingScore >= 80)(personnel)
+const mapAverage = (personnel) => mapCurry((person)=> {
+    return (person.shootingScore + person.pilotingScore) / 2 
+})(personnel)
+
 // console.log(defaultCurry({
 //     http: 403,
 //     connection: 'close',
