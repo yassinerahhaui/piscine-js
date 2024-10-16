15cd2cd9c7b279bc7206489a5e146098dc6e1223
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-16 23:47:41 +0100

    test

--- replica.js
+++ replica.js
@@ -1,6 +1,6 @@
-const replica = (...objects) => {
+const replica = (...args) => {
     const result = {}
-    objects.forEach(obj => {
+    args.forEach(obj => {
         Object.keys(obj).forEach(key => {
             if (typeof obj[key] === "object" && typeof result[key] === "object" && !Array.isArray(obj[key]) && !Array.isArray(result[key])){
                 result[key] = {...result[key], ...obj[key]}
