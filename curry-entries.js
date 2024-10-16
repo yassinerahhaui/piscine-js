4828b3a7788c0ba2df6b3490bac6c19bed7a5e2d
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-16 17:33:33 +0100

    curry-entries

--- curry-entries.js
+++ curry-entries.js
@@ -1,19 +1,19 @@
 const defaultCurry = (obj1) => (obj2) => {
-    const result = {...obj1}
-    for (let [key,val] of Object.entries(obj2)) {
+    const result = { ...obj1 }
+    for (let [key, val] of Object.entries(obj2)) {
         result[key] = val;
     }
     return result
 }
 const mapCurry = (func) => (obj) => {
     const result = {}
-    for (let [key,val] of Object.entries(obj)) {
-        [key,val] = func([key,val])
+    for (let [key, val] of Object.entries(obj)) {
+        [key, val] = func([key, val])
         result[key] = val
     }
     return result
 }
-const reduceCurry = (func) => (obj,init) => {
+const reduceCurry = (func) => (obj, init) => {
     let result = init
     const arr = Object.entries(obj)
     if (typeof init === 'undefined') {
@@ -35,26 +35,37 @@ const filterCurry = (func) => (obj) => {
     return res
 }
 
-// const personnel = {
-//     lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
-//     sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
-//     zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
-//     ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
-//     calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
-//   }
-  
+const personnel = {
+    lukeSkywalker: { id: 5, pilotingScore: 98, shootingScore: 56, isForceUser: true },
+    sabineWren: { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+    zebOrellios: { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+    ezraBridger: { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true },
+    calebDume: { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true },
+}
+const reduceValues = (obj, func, init) => {
+    let firstkey = Object.keys(obj)[0]
+    let result = init || obj[firstkey]
+    for (let [key, val] of Object.entries(obj)) {
+        if (typeof init === 'undefined' && key === firstkey) {
+            continue
+        }
+        result = func(result, val, key, obj)
+    }
+    return result
+}
 
-const reduceScore = (personnel,acc) => reduceCurry(([key,value],acc) => {
-    if (key === 'isForceUser' && value === true) {
-        acc =  value.pilotingScore + value.shootingScore
+const reduceScore = (personnel, acc) => reduceValues(personnel, (acc, val , key )=>{
+    if (val.isForceUser) {
+        acc = val.pilotingScore + val.shootingScore
     }
     return acc
-})(personnel,acc)
-// console.log(reduceScore(personnel));
+    // return 0 
+}, acc)
+console.log(reduceScore(personnel,0));
 
-const filterForce = (personnel) => filterCurry((person)=> person.shootingScore >= 80)(personnel)
-const mapAverage = (personnel) => mapCurry((person)=> {
-    return (person.shootingScore + person.pilotingScore) / 2 
+const filterForce = (personnel) => filterCurry((person) => person.shootingScore >= 80)(personnel)
+const mapAverage = (personnel) => mapCurry((person) => {
+    return (person.shootingScore + person.pilotingScore) / 2
 })(personnel)
 
 // console.log(defaultCurry({
