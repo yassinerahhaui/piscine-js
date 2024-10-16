978ed861260fa60ad755e1596e186fbfa42eaac0
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-16 20:27:15 +0100

    deep-target

--- deep-copy.js
+++ deep-copy.js
@@ -3,7 +3,7 @@ const deepCopy = (target) => {
     if (Array.isArray(target)) {
         result = [...target]
     } else {
-        result = Object.assign(target)
+        result = Object.assign({}, target)
     }
     return result
 }
