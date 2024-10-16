a7fb7fb00b106d8c82744a30612a50fddbd8d25c
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-16 22:14:20 +0100

    deep-copy.js

--- deep-copy.js
+++ deep-copy.js
@@ -1,10 +1,22 @@
-const deepCopy = (target) => {
-    let result;
-    if (Array.isArray(target)) {
-        result = [...target]
-    } else {
-        result =  JSON.parse(JSON.stringify(target))
-        return Object.keys(result).length === 0 ? {...target} : result 
+let result;
+const deepCopy = (obj) => {
+    if (typeof obj !== "object" || typeof obj === null || typeof obj === 'function' || obj instanceof RegExp) {
+        return obj;
+    }
+    const result = Array.isArray(obj) ? [] : {}
+    for (let [key,_] of Object.entries(obj)) {
+        result[key] = deepCopy(obj[key])
     }
     return result
 }
+
+// const obj = {
+//     a: 1,
+//     c: 15,
+//     arr: ["a","b"],
+//     child: {
+//         a: 1,
+//         b: 10
+//     }
+// }
+// console.log(deepCopy(obj));
