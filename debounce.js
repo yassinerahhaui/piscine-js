5d3b5112d1dcdb3eca7a8b6bf4c0afedb3a6cb65
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-17 15:09:52 +0100

    debounce

--- debounce.js
+++ debounce.js
@@ -8,14 +8,39 @@ const debounce = (func, wait) => {
 		}, wait)
 	}
 }
-
-const opDebounce = (func, wait, options = { leading: false }) => {
-	let {leading} = options
+const opDebounce = (func, wait, leading = false) => {
+	let timeout;
+	let called = false
 	return (...args) => {
-		if (leading) {
+		if (leading && !called) {
 			func.apply(this,args)
-		} else {
-			debounce(func(this,args),wait)
+			called = true
+
 		}
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			called = false
+			if (!leading) {
+				func.apply(this,args)
+			}
+		}, wait)
 	}
 }
+
+
+
+
+
+
+
+
+// const opDebounce = (func, wait, options = { leading: false }) => {
+// 	let {leading} = options
+// 	return (...args) => {
+// 		if (leading) {
+// 			func.apply(this,args)
+// 		} else {
+// 			debounce(func(this,args),wait)
+// 		}
+// 	}
+// }
