216bc610bc4c63fc58246d7b09b55e6a78efee73
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-18 18:37:59 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -1,42 +1,26 @@
-const throttle = (func, wait, options = { leading: true, trailing: true }) => {
-    let timeout;
+const throttle = (func, wait) => {
     let called = false;
-    const { leading, trailing } = options;
-
     return (...args) => {
-        if (!timeout) {
-            if (leading) {
-                func.apply(this, args);
-            }
-            called = true; 
-            timeout = setTimeout(() => {
-                if (trailing && called) {
-                    func.apply(this, args);
-                }
-                timeout = null;
-                called = false;
-            }, wait);
-        }
-    };
-};
-
-const opThrottle = (func, wait, options = { leading: false, trailing: false }) => {
-    let timeout;
-    let called = false;
-    const { leading, trailing } = options;
-
-    return (...args) => {
-        if (!called && leading) {
+        if (!called) {
             func.apply(this, args);
-            called = true;
+            called = true
+            timeout = setTimeout(() => {
+                called = false
+            }, wait)
         }
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            if (trailing && called) {
-                func.apply(this, args);
-            }
-            called = false;
-        }, wait);
-    };
+    }
 };
-
+const opThrottle = (func, wait, options = { leading: false, trailing: false }) => {
+    let called = false
+    let { leading, trailing } = options
+    return (...args) => {
+        if (!called) {
+            called = true
+            if (leading || (leading && trailing)) func.apply(this, args);
+            timeout = setTimeout(() => {
+                if (trailing && !leading) func.apply(this, args);
+                called = false
+            }, wait)
+        }
+    }
+}
