faa749dd1a40560e255551ccfe347c3601e37d39
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-18 02:12:35 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -1,26 +1,35 @@
 const throttle = (func, wait) => {
     let timeout;
-    let called = false
-    // let {leading} = options
+    let called = false;
+
     return (...args) => {
-        if (!called) { func.apply(this, args); called = true }
+        if (!called) {
+            func.apply(this, args);
+            called = true;
+        }
         clearTimeout(timeout);
         timeout = setTimeout(() => {
-            if (called) func.apply(this, args);
-            called = false
-        }, wait)
-    }
+            called = false;
+        }, wait);
+    };
 };
-const opThrottle = (func, wait, options = {leading : false,trailing: false}) => {
+
+const opThrottle = (func, wait, options = { leading: false, trailing: false }) => {
     let timeout;
-    let called = false
-    let {leading, trailing} = options
+    let called = false;
+    const { leading, trailing } = options;
+
     return (...args) => {
-        if (!called && leading) { func.apply(this, args); called = true }
+        if (!called && leading) {
+            func.apply(this, args);
+            called = true;
+        }
         clearTimeout(timeout);
         timeout = setTimeout(() => {
-            if (called || trailing) func.apply(this, args);
-            called = false
-        }, wait)
-    }
-}
+            if (trailing && called) {
+                func.apply(this, args);
+            }
+            called = false;
+        }, wait);
+    };
+};
