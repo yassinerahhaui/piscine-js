67ab4e52330fba243bf53ca7d38d89229aee1e17
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-19 04:50:32 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -1,7 +1,7 @@
 const interpolation = (obj) => {
     const durationStep = obj.duration / obj.step
     const delayStep = (obj.end - obj.start) / obj.step
-    let [x, y] = [obj.start , obj.start]
+    let [x, y] = [obj.start , 0]
     const count = setInterval(()=> {
         y += durationStep
         obj.callback([parseFloat(x.toFixed(2)), parseFloat(y.toFixed(2))]);
