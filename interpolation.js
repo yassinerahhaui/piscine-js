031a93a89e584787c40fcf1243e555270cf2e1c7
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-19 01:23:01 +0100

    interpolation

---
+++ interpolation.js
@@ -0,0 +1,29 @@
+const interpolation = (obj) => {
+    obj.callback = function() {
+        let res = []
+        const ds = this.duration / this.step
+        const ts = this.end / this.step
+        let startts = this.start
+        let startds = this.start
+        for (let i = 0; i < this.step;i++) {
+            startds += ds
+            res.push([parseFloat(startts.toFixed(1)),parseFloat(startds.toFixed(1))])
+            startts+= ts
+        }
+        return res
+    }
+    return obj.callback()
+}
+// const obj = {step: 5,start: 0,end: 1,duration: 10}
+// console.log(interpolation(obj))
+// console.log(obj.callback());
+
+/*
+    [
+        [0, 2],
+        [0.2, 4],
+        [0.4, 6],
+        [0.6, 8],
+        [0.8, 10],
+    ]
+*/
