b88abee0d46a1e046dcb30c5d6c5a5dea65671ef
yrahhaou <yassinerahhaoui12@gmail.com>
2024-10-21 17:17:31 +0100

    verydisco

---
+++ verydisco.mjs
@@ -0,0 +1,11 @@
+import {argv} from "node:process"
+
+const arg = argv[2]
+let arr = arg.split(' ')
+let result = arr.map(el=> {
+    let len = el.length
+    let middle = Math.ceil(len/2)
+    return `${el.slice(middle)}${el.slice(0,middle)}`  
+})
+// result = result.join('')
+console.log(result);
